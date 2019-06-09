cbb981e862430f6345b646996bcfaf6e9b0338b5
cz67998 <413874736@qq.com>
2019-06-09 22:29:57 +0800

    redis-cache

--- src/main/java/com/example/rediscache/controller/RedisService.java
+++ src/main/java/com/example/rediscache/controller/RedisService.java
@@ -4,9 +4,7 @@ package com.example.rediscache.controller;
  * Created by IDEA
  * author:wangzhou
  * Date:2019/6/9
- * Time:22:04
+ * Time:22:29
  **/
-public class RdisCacheController {
-    private int number;
-
+public class RedisService {
 }
